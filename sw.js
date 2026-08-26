3335081a205ffebd0a23a9c94500621b9f7c2141
root <root@localhost.localdomain>
2026-08-26 07:55:16 +0000

    Remove old game files from git tracking, keeping local backup

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME='air-combat-v1';
-const urlsToCache=['./','./index.html'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
